e22ff7262549a274694ccd750361c283022f6913
mitko <Mitko.M.S@telco.com>
2023-01-14 13:23:15 +0200

    fixes for project.py

--- api/project.py
+++ api/project.py
@@ -1,7 +1,15 @@
+import json
 import os
 import sys
-from orm import *
 from connexion import request
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from orm import *
+from utils import *
+
+config = json.loads(os.environ['edge_config'])
 
 
 def dump_full(p):
@@ -12,12 +20,104 @@ def dump_full(p):
     }
 
 
-def get_projects():
-    db = Database()
+def get_projects(page=1, count=20, order='name', direction='asc'):
+    try:
+        cred = decode_manager_token(get_value(config, 'token.public_file'), request.headers['X-Auth-Token'])
+    except Exception as ex:
+        return str(ex), 401
+    db = ManagerDB(get_value(config, 'database_engine'), logger.level == logging.DEBUG)
     q = db.session.query(Project)
+    if not get_value(cred, 'is_super_user'):
+        q = q.filter(Project.name == get_value(cred, 'project'))
+    try:
+        Project.__getattribute__(Project, order)
+    except Exception as ex:
+        logger.error(ex)
+        return str(ex), 500
+    if direction == 'asc':
+        q = q.order_by(Project.__getattribute__(Project, order).asc())
+    else:
+        q = q.order_by(Project.__getattribute__(Project, order).desc())
     q_cnt = q.count()
-    for p in q:
-        print(dump_full(p))
-    print(q_cnt)
+    q = q.limit(count).offset((page-1)*count)
+    return {'count': q_cnt, 'data': [p.dump() for p in q]}
 
-get_projects()
+
+def get_project(id):
+    try:
+        cred = decode_manager_token(get_value(config, 'token.public_file'), request.headers['X-Auth-Token'])
+    except Exception as ex:
+        return str(ex), 401
+    db = ManagerDB(get_value(config, 'database_engine'), logger.level == logging.DEBUG)
+    if get_value(cred, 'is_super_user'):
+        res = db.session.query(Project).filter(Project.id == id).one_or_none()
+    else:
+        res = db.session.query(Project).filter(Project.id == id, Project.name == get_value(cred, 'project')).one_or_none()
+    return res.dump() if res is not None else ('Not found', 404)
+
+
+def put_project(project):
+    try:
+        cred = decode_manager_token(get_value(config, 'token.public_file'), request.headers['X-Auth-Token'])
+    except Exception as ex:
+        return str(ex), 401
+    if not get_value(cred, 'is_super_user'):
+        return 'This operation is not allowed!', 403
+    project['name'] = project['name'].strip()
+    if project['name'].find(' ') > -1:
+        return "Project name must be one word.", 403
+    db = ManagerDB(get_value(config, 'database_engine'), logger.level == logging.DEBUG)
+    p = db.session.query(Project).filter(Project.name == project['name']).one_or_none()
+    if p is not None:
+        return "Project with name '{}' already exists!".format(project['name']), 403
+    logger.info('Create project: {}'.format(project))
+    n = Project()
+    update_attributes(n, project)
+    db.session.add(n)
+    db.session.commit()
+    db.session.flush()
+    # Create default values in the database for this project
+    db.initialize_project(project_id=n.id, config=config)
+    return n.id, 201
+
+
+def post_project(id, project):
+    try:
+        cred = decode_manager_token(get_value(config, 'token.public_file'), request.headers['X-Auth-Token'])
+    except Exception as ex:
+        return str(ex), 401
+    if not get_value(cred, 'is_super_user'):
+        return 'This operation is not allowed!', 403
+    project['name'] = project['name'].strip()
+    if project['name'].find(' ') > -1:
+        return "Project name must be one word.", 403
+    logger.info('Update project: {}'.format(project))
+    db = ManagerDB(get_value(config, 'database_engine'), logger.level == logging.DEBUG)
+    p = db.session.query(Project).filter(Project.id == id).one_or_none()
+    if p is None:
+        return "This project does not exists!", 404
+    p_ref = db.session.query(Project).filter(Project.name == project['name'], Project.id != id).one_or_none()
+    if p_ref is not None:
+        return "Project with name '{}' already exists!".format(project['name']), 403
+    update_attributes(p, project)
+    db.session.add(p)
+    db.session.commit()
+    return "Project {} was updated".format(project['name']), 200
+
+
+def delete_project(id):
+    try:
+        cred = decode_manager_token(get_value(config, 'token.public_file'), request.headers['X-Auth-Token'])
+    except Exception as ex:
+        return str(ex), 401
+    if not get_value(cred, 'is_super_user'):
+        return 'This operation is not allowed!', 403
+    if id == 1:
+        return 'This project is administratively protected from deletion!', 403
+    db = ManagerDB(get_value(config, 'database_engine'), logger.level == logging.DEBUG)
+    res = db.session.query(Project).filter(Project.id == id).one_or_none()
+    if res is None:
+        return "This project does not exists!", 404
+    logger.info('Delete project: {}'.format(res))
+    db.delete_data_flow(Project.__tablename__, id)
+    return "Project was deleted", 200
